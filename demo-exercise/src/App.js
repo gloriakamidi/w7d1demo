fcce41fa796d5a3d4038618e8af6e4b265c4845a
gloriakamidi <g.kamidi.312@westcliff.edu>
2023-02-16 00:43:33 -0800

    w7d1demo

--- demo-exercise/src/App.js
+++ demo-exercise/src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/collapsible">Demo 1</Link>
+          </li>
+          <li>
+            <Link to="/vertex">Demo 2</Link>
+          </li>
+          <li>
+            <Link to="/demo3">Demo 3</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/collapsible">
+          <collapsible />
+        </Route>
+        <Route path="/vertex">
+          <vertex />
+        </Route>
+        <Route path="/demo3">
+          <demo3 />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- demo-exercise/src/src  sub2/collapsible.js
+++ demo-exercise/src/src  sub2/collapsible.js
@@ -3,8 +3,8 @@ import { render } from "react-dom";
 import Tree from "./Tree";
 import data from "./data";
 
-const App = () => {
+const Collapsible = () => {
   return <Tree data={data} width={600} height={500} />;
 };
 
-render(<App />, document.getElementById("root"));
+render(<Collapsible />, document.getElementById("root"));
