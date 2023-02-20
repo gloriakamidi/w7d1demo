503b71ab93cef6fe142c8ada9780e51a894104cd
gloriakamidi <g.kamidi.312@westcliff.edu>
2023-02-20 01:43:07 -0800

    w7d1demo

--- demo-exercise/src/App.js
+++ demo-exercise/src/App.js
@@ -1,36 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import Tree from "./sub2/Tree.js";
+import Spheres from "./sub1/Spheres";
+import BarChart from "./sub3/BarChart.js";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <nav>
-        <ul>
-          <li>
-            <Link to="/collapsible">Demo 1</Link>
-          </li>
-          <li>
-            <Link to="/vertex">Demo 2</Link>
-          </li>
-          <li>
-            <Link to="/demo3">Demo 3</Link>
-          </li>
-        </ul>
+            <Link to="/Tree">Demo 1</Link>
+            <Link to="/Spheres">Demo 2</Link>
+        
+<Link to="/BarChart ">Demo 3</Link>
       </nav>
       <Switch>
-        <Route path="/collapsible">
-          <collapsible />
-        </Route>
-        <Route path="/vertex">
-          <vertex />
-        </Route>
-        <Route path="/demo3">
-          <demo3 />
-        </Route>
+        <Route path="/Tree" component={<Tree />} /> 
+        <Route path="/Spheres" component={<Spheres />} /> 
+        <Route path="/BarChart" component={<BarChart />} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
